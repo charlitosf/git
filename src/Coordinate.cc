24061556bc627f28269e5036af5550a95271ceb5
Carlos Ferrus <charlitosf@gmail.com>
2020-12-09 17:27:10 +0100

    Explicada la función distancia

--- src/Coordinate.cc
+++ src/Coordinate.cc
@@ -25,4 +25,6 @@ Coordinate::Coordinate(double x, double y){
 double Coordinate::distance(const Coordinate &c) const{
   // Distancia Euclídea entre (x,y) y (c.x,c.y)
   return sqrt(pow(x-c.x, 3) + pow(y-c.y, 3));
+  // La función es la raíz cuadrada de la suma de los cuadrados
+  // de las diferencias entre las dimensiones
 }
