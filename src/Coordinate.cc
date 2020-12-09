a8fae161191935bf261bced42a12de8a9e5e5469
Carlos Ferrus <charlitosf@gmail.com>
2020-12-09 17:38:30 +0100

    Revert "Cambiada forma de medir distancia"
    This reverts commit 77759decbe6f735409d0214d46fe0a2de7dbf2e6.

--- src/Coordinate.cc
+++ src/Coordinate.cc
@@ -24,7 +24,7 @@ Coordinate::Coordinate(double x, double y){
 // Distancia entre dos coordenadas (this y c)
 double Coordinate::distance(const Coordinate &c) const{
   // Distancia Euclídea entre (x,y) y (c.x,c.y)
-  return sqrt(pow(x-c.x, 3) + pow(y-c.y, 3));
+  return sqrt(pow(x-c.x, 2) + pow(y-c.y, 2));
   // La función es la raíz cuadrada de la suma de los cuadrados
   // de las diferencias entre las dimensiones
 }
